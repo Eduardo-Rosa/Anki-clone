b9fa3ce5be15591acd0c75143b0902d4abf78c54
Eduardo Betim Rosa <edu.betim@aol.com>
2026-09-07 12:19:48 -0300

    inicial

---
+++ src/main/java/com/rosa_eduardo/anki_clone/AnkiCloneApplication.java
@@ -0,0 +1,13 @@
+package com.rosa_eduardo.anki_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkiCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnkiCloneApplication.class, args);
+	}
+
+}
